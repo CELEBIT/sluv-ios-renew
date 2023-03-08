ec21b8c6a19af3db4cda0ff81679b85c43ebf134
ezenjun <ezenjun07@gmail.com>
2023-03-08 14:34:36 +0900

    Initial Commit

---
+++ sluv-ios-renew/sluv_ios_renewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sluv_ios_renewApp.swift
+//  sluv-ios-renew
+//
+//  Created by JunYong Park on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct sluv_ios_renewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
